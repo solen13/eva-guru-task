560b9702264caf9cbe3f8da604548697b59c7b04
solen13 <uusolen13@gmail.com>
2025-02-05 01:28:18 +0300

    highcharts integrated

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue';
 import './assets/style.css';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+import HighchartsVue from 'highcharts-vue';
+
+const app = createApp(App);
+
+// HighchartsVue'u Vue uygulamasına dahil et
+app.use(HighchartsVue);
+
+// Uygulamayı mount et
+app.mount('#app');
